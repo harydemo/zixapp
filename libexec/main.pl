3e3debbb4c312bbe1aa7c8165055fb0cf05427e8
hary <zcman2005@gmail.com>
2013-03-22 01:27:01 +0800

    main.pl changed

--- libexec/main.pl
+++ libexec/main.pl
@@ -4,15 +4,18 @@ use Zeta::Run;
 use Zeta::IPC::MsgQ;
 
 use POE;
-use Zeta::Run::Main::HTTP;
+use Zeta::POE::HTTPD;
 
 sub {
     my $cfg = zkernel->zapp_config()->{main};
-    Zeta::Run::Main::HTTP->run( 
+    Zeta::POE::HTTPD->spawn( 
          port   => $cfg->{port}, 
          module => $cfg->{module}, 
          para   => $cfg->{para}
     );
+    $poe_kernel->run();
+    zkernel->process_stopall();
+    exit 0;
 };
 
 __END__
